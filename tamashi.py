9250429fecb6f6e846b34d3e880f0ae76b8f7780
zelima <ir.mchedlishvili@gmail.com>
2021-01-12 14:39:46 +0400

    link for excercises

--- tamashi.py
+++ tamashi.py
@@ -1,4 +1,4 @@
-
+# https://www.practicepython.org/
 def guess():
     game_range = 1000
     zero = 0
@@ -8,7 +8,7 @@ def guess():
         second_part = range(int(game_range/2),game_range)
 
         is_greater = input('Is it greater than %s?'%(game_range/2))
-        
+
         if is_greater == 'y':
             zero = (game_range - zero)/2
         elif is_greater == 'n':
